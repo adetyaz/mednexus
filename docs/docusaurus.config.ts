cd883c2d4737c7868683b2d51da02f64233369bd
adetyaz <tuneymitch@gmail.com>
2025-08-21 13:46:23 +0100

    .

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://mednexus-docs.netlify.app", // Replace with your actual Netlify URL
+  url: "https://mednexus.netlify.app/", 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
